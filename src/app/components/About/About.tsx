bfdeb53d3f4f877c3151348a1ff393ffc245975a
Jhonatan Bolivar <jhonatanbolivar14@gmail.com>
2023-10-31 14:51:44 -0500

    about section added

--- src/app/components/About/About.tsx
+++ src/app/components/About/About.tsx
@@ -1,20 +1,27 @@
+'use client'
+import { RevealWrapper } from 'next-reveal'
 
 export function About() {
     return(
         <div className="w-screen pt-20 px-44 max-xl:px-20 max-md:px-5 flex flex-row-reverse max-lg:flex-col gap-10 items-center justify-between">
-            <img className="max-w-lg rounded-xl max-xl:max-w-sm" src="https://res.cloudinary.com/da3li8fso/image/upload/v1698112231/ijgdjpy2bhhlaxxk3hym.png" alt="" />
+
+            <RevealWrapper origin='left'>
+                <img className="max-w-lg rounded-xl max-xl:max-w-sm" src="https://res.cloudinary.com/da3li8fso/image/upload/v1698112231/ijgdjpy2bhhlaxxk3hym.png" alt="" />
+            </RevealWrapper>
             
-            <p className="font-['Poppins'] text-xl max-sm:text-base max-w-xl px-7 max-sm:px-0 text-slate-700 dark:text-slate-400">Soy un apasionado desarrollador full stack con una amplia experiencia en la creación 
-                de soluciones web de extremo a extremo. Mi conjunto de habilidades abarca tanto el
-                desarrollo frontend como backend, lo que me permite diseñar y construir aplicaciones
-                web altamente funcionales y atractivas. Mi enfoque en la optimización del rendimiento, 
-                la seguridad y la usabilidad me ha permitido entregar proyectos de alta calidad que
-                cumplen con las expectativas de los clientes y usuarios. Estoy constantemente actualizándome
-                y adoptando las últimas tecnologías, lo que me permite mantenerme al día con las tendencias
-                de desarrollo y ofrecer soluciones innovadoras. Además, mi capacidad para trabajar de manera
-                colaborativa en equipos multidisciplinarios me permite abordar desafíos de manera efectiva y
-                entregar resultados excepcionales
-            </p>
+            <RevealWrapper origin='right'>
+                <p className="font-['Poppins'] text-xl max-sm:text-base max-w-xl px-7 max-sm:px-0 text-slate-700 dark:text-slate-400">Soy un apasionado desarrollador full stack con una amplia experiencia en la creación 
+                    de soluciones web de extremo a extremo. Mi conjunto de habilidades abarca tanto el
+                    desarrollo frontend como backend, lo que me permite diseñar y construir aplicaciones
+                    web altamente funcionales y atractivas. Mi enfoque en la optimización del rendimiento, 
+                    la seguridad y la usabilidad me ha permitido entregar proyectos de alta calidad que
+                    cumplen con las expectativas de los clientes y usuarios. Estoy constantemente actualizándome
+                    y adoptando las últimas tecnologías, lo que me permite mantenerme al día con las tendencias
+                    de desarrollo y ofrecer soluciones innovadoras. Además, mi capacidad para trabajar de manera
+                    colaborativa en equipos multidisciplinarios me permite abordar desafíos de manera efectiva y
+                    entregar resultados excepcionales
+                </p>
+            </RevealWrapper>
         </div>
     )
 }
